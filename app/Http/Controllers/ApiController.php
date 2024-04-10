7baef1fd05b0cfce45a8d346053f5cd3a2c42120
luonverak <112792376+luonverak@users.noreply.github.com>
2024-04-10 21:46:01 +0700

    code commit

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,16 +7,48 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    public function getStudent(){
+    public function getStudent()
+    {
         $student = Student::all();
-        if($student->count() > 0){
-            return response()->json($student);
-        }else{
+        if ($student->count() > 0) {
             return response()->json(
                 [
-                    'status' =>'No data'
+                    'status' => 'success',
+                    'student' => $student
                 ]
             );
+        } else {
+            return response()->json(
+                [
+                    'status' => 'No data'
+                ]
+            );
+        }
+    }
+    public function addStudent(Request $request)
+    {
+        try {
+            $file = $request->file('profile');
+            $fileName = rand(1, 1000) . '-' . $file->getClientOriginalName();
+            $file->move('images', $fileName);
+            $student = Student::create(
+                [
+                    'name' => $request->name,
+                    'gender' => $request->gender,
+                    'profile' => $fileName,
+                ]
+            );
+            if ($student) {
+                return response()->json([
+                    'stutus' => 'Success'
+                ]);
+            } else {
+                return response()->json([
+                    'stutus' => 'Error'
+                ]);
+            }
+        } catch (\Throwable $th) {
+            throw $th;
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 
-Route::get('/student',[ApiController::class,'getStudent']);
+Route::get('/student', [ApiController::class, 'getStudent']);
+Route::post('/add', [ApiController::class, 'addStudent']);
